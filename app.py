0f79866de8169f88dd123232f45a3c0a2a0ed390
gaikwadjk <152171452+gaikwadjk@users.noreply.github.com>
2024-03-14 16:36:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from flask import Flask, redirect, url_for, request, render_template, session
 app = Flask(__name__)
 
 mydb = mysql.connector.connect(
-  host="ai-db.mysql.database.azure.com",
+  host="translator-app.mysql.database.azure.com",
   user="redblack",
-  password="Password@123",
+  password="Password#123",
   database="ai"
 )
 
@@ -37,7 +37,7 @@ def index_post():
     #endpoint = os.environ['ENDPOINT']
     #location = os.environ['LOCATION']
 
-    key="94a5ad7bbedd4e6f833ec571c6d4b8a3"
+    key="8008dca836474bb382bc745aee3b0b81"
     endpoint="https://api.cognitive.microsofttranslator.com/"
     location="eastus"
 
